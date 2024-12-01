fb869a35cc0264e07ce52386ddd3bc7096fe807e
Retr0 <dedsecretr0680@gmail.com>
2024-12-01 23:40:32 +0530

    idk

--- go.mod
+++ go.mod
@@ -2,4 +2,49 @@ module github.com/omriharel/deej
 
 go 1.23.1
 
-require github.com/moutend/go-wca v0.3.0 // indirect
+require (
+	github.com/akavel/rsrc v0.10.2 // indirect
+	github.com/cratonica/2goarray v0.0.0-20190331194516-514510793eaa // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/gen2brain/beeep v0.0.0-20240516210008-9c006672e7f4 // indirect
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/getlantern/systray v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jacobsa/go-serial v0.0.0-20180131005756-15cf729a72d4 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/moutend/go-wca v0.2.0 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.19.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	github.com/thoas/go-funk v0.9.3 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/term v0.26.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- pkg/deej/cmd/main.go
+++ pkg/deej/cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 
 	"github.com/omriharel/deej/pkg/deej"
 )
@@ -12,59 +11,56 @@ var (
 	gitCommit  string
 	versionTag string
 	buildType  string
-	verbose    bool
+
+	verbose bool
 )
 
 func init() {
-	// Consolidate verbose flag definition
-	flag.BoolVar(&verbose, "verbose", false, "Show verbose logs (useful for debugging serial)")
-	flag.BoolVar(&verbose, "v", false, "Shorthand for --verbose")
+	flag.BoolVar(&verbose, "verbose", false, "show verbose logs (useful for debugging serial)")
+	flag.BoolVar(&verbose, "v", false, "shorthand for --verbose")
 	flag.Parse()
 }
 
 func main() {
-	// First we need a logger
+
+	// first we need a logger
 	logger, err := deej.NewLogger(buildType)
 	if err != nil {
-		fmt.Printf("Failed to create logger: %v\n", err)
-		os.Exit(1) // Exit with error status
+		panic(fmt.Sprintf("Failed to create logger: %v", err))
 	}
 
-	// Named logger for the 'main' function
 	named := logger.Named("main")
 	named.Debug("Created logger")
 
-	// Log version info
-	if versionTag != "" || gitCommit != "" {
-		named.Infow("Version info", "gitCommit", gitCommit, "versionTag", versionTag, "buildType", buildType)
-	}
+	named.Infow("Version info",
+		"gitCommit", gitCommit,
+		"versionTag", versionTag,
+		"buildType", buildType)
 
-	// Provide a fair warning if the user's running in verbose mode
+	// provide a fair warning if the user's running in verbose mode
 	if verbose {
-		named.Debug("Verbose mode enabled, all log messages will be shown")
-		// Set logger to verbose level for more detailed logging if needed
+		named.Debug("Verbose flag provided, all log messages will be shown")
 	}
 
-	// Create the deej instance
+	// create the deej instance
 	d, err := deej.NewDeej(logger, verbose)
 	if err != nil {
-		named.Fatalw("Failed to create deej instance", "error", err)
+		named.Fatalw("Failed to create deej object", "error", err)
 	}
 
-	// Set version info for tray (if available)
-	if versionTag != "" || gitCommit != "" {
-		versionIdentifier := versionTag
-		if versionIdentifier == "" {
-			versionIdentifier = gitCommit
+	// if injected by build process, set version info to show up in the tray
+	if buildType != "" && (versionTag != "" || gitCommit != "") {
+		identifier := gitCommit
+		if versionTag != "" {
+			identifier = versionTag
 		}
-		versionString := fmt.Sprintf("Version %s-%s", buildType, versionIdentifier)
+
+		versionString := fmt.Sprintf("Version %s-%s", buildType, identifier)
 		d.SetVersion(versionString)
 	}
 
-	// Initialize deej
-	if err := d.Initialize(); err != nil {
+	// onwards, to glory
+	if err = d.Initialize(); err != nil {
 		named.Fatalw("Failed to initialize deej", "error", err)
 	}
-
-	named.Info("Deej initialized successfully")
 }
