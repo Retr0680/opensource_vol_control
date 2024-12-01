dc393e8ec510b5d65ad5fa5b41d562d00a24faea
Retr0 <dedsecretr0680@gmail.com>
2024-12-01 23:22:44 +0530

    Update main.go

--- pkg/deej/cmd/main.go
+++ pkg/deej/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/omriharel/deej/pkg/deej"
 )
@@ -11,56 +12,59 @@ var (
 	gitCommit  string
 	versionTag string
 	buildType  string
-
-	verbose bool
+	verbose    bool
 )
 
 func init() {
-	flag.BoolVar(&verbose, "verbose", false, "show verbose logs (useful for debugging serial)")
-	flag.BoolVar(&verbose, "v", false, "shorthand for --verbose")
+	// Consolidate verbose flag definition
+	flag.BoolVar(&verbose, "verbose", false, "Show verbose logs (useful for debugging serial)")
+	flag.BoolVar(&verbose, "v", false, "Shorthand for --verbose")
 	flag.Parse()
 }
 
 func main() {
-
-	// first we need a logger
+	// First we need a logger
 	logger, err := deej.NewLogger(buildType)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to create logger: %v", err))
+		fmt.Printf("Failed to create logger: %v\n", err)
+		os.Exit(1) // Exit with error status
 	}
 
+	// Named logger for the 'main' function
 	named := logger.Named("main")
 	named.Debug("Created logger")
 
-	named.Infow("Version info",
-		"gitCommit", gitCommit,
-		"versionTag", versionTag,
-		"buildType", buildType)
-
-	// provide a fair warning if the user's running in verbose mode
-	if verbose {
-		named.Debug("Verbose flag provided, all log messages will be shown")
+	// Log version info
+	if versionTag != "" || gitCommit != "" {
+		named.Infow("Version info", "gitCommit", gitCommit, "versionTag", versionTag, "buildType", buildType)
 	}
 
-	// create the deej instance
+	// Provide a fair warning if the user's running in verbose mode
+	if verbose {
+		named.Debug("Verbose mode enabled, all log messages will be shown")
+		// Set logger to verbose level for more detailed logging if needed
+	}
+
+	// Create the deej instance
 	d, err := deej.NewDeej(logger, verbose)
 	if err != nil {
-		named.Fatalw("Failed to create deej object", "error", err)
+		named.Fatalw("Failed to create deej instance", "error", err)
 	}
 
-	// if injected by build process, set version info to show up in the tray
-	if buildType != "" && (versionTag != "" || gitCommit != "") {
-		identifier := gitCommit
-		if versionTag != "" {
-			identifier = versionTag
+	// Set version info for tray (if available)
+	if versionTag != "" || gitCommit != "" {
+		versionIdentifier := versionTag
+		if versionIdentifier == "" {
+			versionIdentifier = gitCommit
 		}
-
-		versionString := fmt.Sprintf("Version %s-%s", buildType, identifier)
+		versionString := fmt.Sprintf("Version %s-%s", buildType, versionIdentifier)
 		d.SetVersion(versionString)
 	}
 
-	// onwards, to glory
-	if err = d.Initialize(); err != nil {
+	// Initialize deej
+	if err := d.Initialize(); err != nil {
 		named.Fatalw("Failed to initialize deej", "error", err)
 	}
+
+	named.Info("Deej initialized successfully")
 }
